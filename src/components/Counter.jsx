3baae473b6b507ebf5ec393ca88825763556c43b
Gülüzar KARAKOÇ <karakoc.guluzar99@gmail.com>
2024-12-21 14:12:11 +0300

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+const Counter = ({ initialValue = 0 }) => {
+  const [count, setCount] = useState(initialValue);
+  const [step, setStep] = useState(1);
+
+  useEffect(() => {
+    localStorage.setItem("counterValue", count);
+  }, [count]);
+
+  useEffect(() => {
+    const savedCount = localStorage.getItem("counterValue");
+    if (savedCount) {
+      setCount(Number(savedCount));
+    }
+  }, []);
+  return (
+    <div >
+      <h1>Sayaç Uygulaması</h1>
+      <h2>Mevcut Değer: {count}</h2>
+      <div>
+        <button onClick={() => setCount(count + step)}>+ Artır</button>
+        <button onClick={() => setCount(count - step)}>- Azalt</button>
+        <button onClick={() => setCount(0)}>Sıfırla</button>
+      </div>
+      <div>
+        <label>
+          İşlem Değeri:{" "}
+          <input
+            type="number"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}/>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
